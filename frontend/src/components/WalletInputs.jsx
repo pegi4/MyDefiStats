cc71fa4e9c9b2cad0c20462539e0b87ee4f198f4
Vito Senič <vito.senic@gmail.com>
2023-05-11 17:37:54 +0200

    web3 pack, font color, back and header

--- frontend/src/components/WalletInputs.jsx
+++ frontend/src/components/WalletInputs.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import "../App.css"
+import {Input, Select, CryptoLogos} from '@web3uikit/web3'
 
 function WalletInputs({chain, setChain, wallet, setWallet}) {
+  return (
+    <>
+        <div className='header'>
+          <div className='title'>
+            <svg width="40" height="40" viewBox="0 0 500 500" fill="none" xmlns="http://www.w3.org/2000/svg" style={{verticalAlign:'top'}}>
+            <path id="logo_exterior" d="M500 250C500 111.929 388.071 0 250 0C111.929 0 0 111.929 0 250C0 388.071 111.929 500 250 500C388.071 500 500 388.071 500 250Z" fill="#784FFE"></path>
+            <path id="logo_interior" fill-rule="evenodd" clip-rule="evenodd" d="M154.338 187.869L330.605 187L288.404 250.6L388 250.118L345.792 312.652L168.382 313.787L211.25 250.633L112 250.595L154.338 187.869Z" fill="white"></path>
+            </svg>
+            <h1>My Defi Stats</h1>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default WalletInputs
+
+/* function WalletInputs({chain, setChain, wallet, setWallet}) {
   return (
     <>
         <h1> Input a Wallet and Chain</h1>
@@ -22,4 +42,4 @@ function WalletInputs({chain, setChain, wallet, setWallet}) {
   )
 }
 
-export default WalletInputs
+export default WalletInputs */
