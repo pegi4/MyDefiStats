c5cc92be200d83871e85290e60a01dae0b457676
Vito Senič <vito.senic@gmail.com>
2023-05-18 18:38:17 +0200

    TransferHistory Data Output

--- frontend/src/components/TransferHistory.jsx
+++ frontend/src/components/TransferHistory.jsx
@@ -1,70 +1,91 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import axios from 'axios'
-import { Table } from '@web3uikit/core';
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 import ReactLoading from "react-loading";
+import "./TransferHistory.css";
 
-function TransferHistory({chain, wallet, transfers, setTransfers}) {
-    
-    // Add a new piece of state here
-    const [isLoading, setIsLoading] = useState(true);
+function TransferHistory({ chain, wallet, transfers, setTransfers }) {
+  const [isLoading, setIsLoading] = useState(true);
 
-    const getTokenTransfers = useCallback(async () => {
-        setIsLoading(true); // Set loading to true when the function is called
+  const getTokenTransfers = useCallback(async () => {
+    setIsLoading(true);
 
-        const response = await axios.get("http://localhost:5000/tokenTransfers", {
-            params: {
-                address: wallet,
-                chain: chain,
-            },
-        });
+    const response = await axios.get("http://localhost:5000/tokenTransfers", {
+      params: {
+        address: wallet,
+        chain: chain,
+      },
+    });
 
-        if (response.data) {
-            setTransfers(response.data);
-        }
+    if (response.data) {
+      setTransfers(response.data);
+    }
 
-        setIsLoading(false);
-    }, [wallet, chain, setTransfers]);
+    setIsLoading(false);
+  }, [wallet, chain, setTransfers]);
 
-    useEffect(() => {
-        if(wallet){
-            getTokenTransfers().catch((err) => {
-                console.error(err);
-            });
-        }
-    }, [wallet, chain, getTokenTransfers]);
-  
-    return (
-        <>
-            <div className="tabHeading"> Transfer History </div>
-            <div>
-            {isLoading ? (
-                <ReactLoading type="cylon" color="#687994"
-                    height={100} width={50} /> 
-            ) : transfers.length > 0 ? (
-                <Table
-                    pageSize={8}
-                    noPagination={true}
-                    style={{ width: 'auto' }}
-                    columnsConfig='auto auto auto auto auto'
-                    data={transfers.map((e) => [
-                        e.symbol,
-                        (Number(e.value) / Number(`1e${e.decimals}`)).toFixed(2),
-                        `${e.from_address.slice(0,4)}...${e.from_address.slice(38)}`,
-                        `${e.to_address.slice(0,4)}...${e.to_address.slice(38)}`,
-                        e.block_timestamp.slice(0,10),
-                    ])}
-                    header={[
-                        <span>Token</span>,
-                        <span>Amount</span>,
-                        <span>From</span>,
-                        <span>To</span>,
-                        <span>Date</span>,
-                    ]}
-                />
-            ) : null}
+  useEffect(() => {
+    if (wallet) {
+      getTokenTransfers().catch((err) => {
+        console.error(err);
+      });
+    }
+  }, [wallet, chain, getTokenTransfers]);
+
+  const groupedTransfers = transfers.reduce((acc, transfer) => {
+    const date = transfer.block_timestamp.slice(0, 10);
+
+    const existingDateIndex = acc.findIndex((group) => group.date === date);
+
+    if (existingDateIndex !== -1) {
+      acc[existingDateIndex].transfers.push(transfer);
+    } else {
+      acc.push({
+        date,
+        transfers: [transfer],
+      });
+    }
+
+    return acc;
+  }, []);
+
+
+  return (
+    <>
+      <div className="tabHeading">Transfer History</div>
+      <div className="transactions-container">
+        {isLoading ? (
+          <ReactLoading type="cylon" color="#687994" height={100} width={50} />
+        ) : (
+          groupedTransfers.map((group) => (
+            <div className="transaction-group" key={group.date}>
+              <div className="date-header">{group.date}</div>
+              {group.transfers.map((transfer) => {
+                return (
+                  <div className="transaction" key={transfer.transaction_hash}>
+                    <div className="transaction-type">
+                    {transfer.from_address.toLowerCase() === wallet.toLowerCase() ? "Sent" : "Received"}
+                    </div>
+                    <div className="token-amount">
+                      {transfer.symbol}:{" "}
+                      {(
+                        Number(transfer.value) /
+                        Number(`1e${transfer.decimals}`)
+                      ).toFixed(2)}
+                    </div>
+                    <div className="address">
+                    {transfer.from_address.toLowerCase() === wallet.toLowerCase()
+                        ? `To: ${transfer.to_address}`
+                        : `From: ${transfer.from_address}`}
+                    </div>
+                  </div>
+                );
+              })}
             </div>
-        </>
-    )
+          ))
+        )}
+      </div>
+    </>
+  );
 }
 
 export default TransferHistory;
