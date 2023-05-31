d4a7127d185d2ce883807cdd47b5e23788471271
Vito Senič <vito.senic@gmail.com>
2023-05-31 11:19:54 +0200

    Handle wallet connection status

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -1,14 +1,25 @@
 import axios from "axios";
+import { useState } from 'react';
 
 export default function User({ session, onLogout, setWallet }) {
+
+  // Wallet connection status
+  const [walletConnected, setWalletConnected] = useState(false);
+
   async function signOut() {
     await axios(`${process.env.REACT_APP_SERVER_URL}/logout`, {
       withCredentials: true,
     });
+    setWalletConnected(false);
+    console.log("Wallet connect: ", walletConnected);
     onLogout();
   }
 
-  setWallet(session.address);
+  if(!walletConnected) {
+    setWallet(session.address);
+    setWalletConnected(true);
+    console.log("Wallet connect: ", walletConnected);
+  }
 
   return (
     <div>
